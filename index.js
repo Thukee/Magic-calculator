1d7397edc582d6b4d7d389bb51a2f196298b0c9d
Thushara Abeysinghe <thukee@gmail.com>
2024-02-08 23:54:42 +0200

    Adding index file

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-let text;
-let num1 = 0;
-let num2 = 0;
-function addition(num1, num2) {
+let operation = prompt("Operation:");
+let num1 = Number(prompt("First number:"));
+let num2 = Number(prompt("Second number:"));
+
+function add(num1, num2) {
   return `${num1} plus ${num2} is equals to ${num1 + num2}`;
 }
 
-console.log(addition(4, 8));
+if (operation == "add") {
+  console.log(add(num1, num2));
+}
