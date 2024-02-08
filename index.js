31d9a8eab612771fb72873f8aa2e3a6e85dbf4e1
Thushara Abeysinghe <thukee@gmail.com>
2024-02-09 00:51:52 +0200

    Loop done

--- index.js
+++ index.js
@@ -14,35 +14,43 @@ while (continueCalculating) {
   }
   let num1 = Number(prompt("First number:"));
   let num2 = Number(prompt("Second number:"));
+
+  switch (operation) {
+    case "add":
+      console.log(add(num1, num2));
+      break;
+    case "sub":
+      console.log(sub(num1, num2));
+      break;
+    case "multi":
+      console.log(multi(num1, num2));
+      break;
+    case "div":
+      console.log(div(num1, num2));
+      break;
+    case "exit":
+      console.log("Aborting. Goodbye!");
+      continueCalculating = false;
+      break;
+    default:
+      console.log("Invalid operation. Please try again.");
+  }
 }
 
 //Arithmatic functions coding starts here//
 
-// function add(num1, num2) {
-//   return `${num1} plus ${num2} is equals to ${num1 + num2}`;
-// }
+function add(num1, num2) {
+  return `${num1} plus ${num2} is equals to ${num1 + num2}`;
+}
 
-// function sub(num1, num2) {
-//   return `${num1} minus ${num2} is equals to ${num1 - num2}`;
-// }
+function sub(num1, num2) {
+  return `${num1} minus ${num2} is equals to ${num1 - num2}`;
+}
 
-// function multi(num1, num2) {
-//   return `${num1} multiply by ${num2} is equals to ${num1 * num2}`;
-// }
+function multi(num1, num2) {
+  return `${num1} multiply by ${num2} is equals to ${num1 * num2}`;
+}
 
-// function div(num1, num2) {
-//   return `${num1} devide by ${num2} is equals to ${num1 / num2}`;
-// }
-
-// if (operation == "add") {
-//   console.log(add(num1, num2));
-// }
-// if (operation == "sub") {
-//   console.log(sub(num1, num2));
-// }
-// if (operation == "multi") {
-//   console.log(multi(num1, num2));
-// }
-// if (operation == "div") {
-//   console.log(div(num1, num2));
-// }
+function div(num1, num2) {
+  return `${num1} devide by ${num2} is equals to ${num1 / num2}`;
+}
